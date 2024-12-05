b6638a40250c6227de005628a56404618c2b6e8e
Nada Al-Doubaie <nadoaldobaie@gmail.com>
2024-12-05 14:39:46 +0300

    first commit

--- lib/utils/nada_app_colors.dart
+++ lib/utils/nada_app_colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class NadaAppColors {
-  static const nadaPrimary = Color(0xFF6366F1);
+  static const nadaPrimary = Color.fromARGB(255, 53, 54, 127);
   static const nadaBackground = Color(0xFFF8FAFC);
   static const nadaWhite = Colors.white;
   static const nadaTextPrimary = Color(0xFF1E293B);
   static const nadaTextSecondary = Color(0xFF64748B);
   static const nadaHover = Color(0xFF818CF8);
 
-  static const nadaGradientStart = Color(0xFF6366F1);
-  static const nadaGradientEnd = Color(0xFF8B5CF6);
+  static const nadaGradientStart = Color.fromARGB(255, 53, 54, 127);
+  static const nadaGradientEnd = Color.fromARGB(255, 152, 122, 222);
 } 
